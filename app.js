317d0332bd369f96e8fe1ef03faee08fec6e4876
sonam_rinzin <sonamrinzingurung@gmail.com>
2022-10-12 22:07:39 +0545

    set up for deploy

--- app.js
+++ app.js
@@ -4,14 +4,33 @@ import dotenv from "dotenv";
 dotenv.config();
 import morgan from "morgan";
 import "express-async-error";
+
+//security
+import helmet from "helmet";
+import xss from "xss-clean";
+
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
 import quoteRoute from "./server/routes/quoteRouter.js";
 import notFound from "./server/error/notFoundError.js";
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
 app.use(express.json());
+app.use(helmet()); //secure headers
+app.use(xss()); //sanitize input , prevent cross site scripting
+
 app.use("/api/v1/ye-said", quoteRoute);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.use(notFound);
 
